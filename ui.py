cd90ea33f99a219a6891d45ca8475450d6b0696e
alexandrezurcher <60798271+alexandrezurcher@users.noreply.github.com>
2022-10-28 11:35:50 +0100

    added the GUI

--- ui.py
+++ ui.py
@@ -1,2 +1,31 @@
+from tkinter import *
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Computer Science QUIZZZ")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score = Label(text=f"Score: {'0'}", bg=THEME_COLOR, fg="white",font=("Arial", 20, "normal"))
+        self.score.grid(column=1, row=0)
+        self.score.config(padx=20)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="test",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="./images/true.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_img = PhotoImage(file="./images/false.png")
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
